7eee45c06248ecc97906ab9ee219df248fbf19cd
david_schilmoeller <dschilmoeller@gmail.com>
2025-12-09 11:19:46 -0800

    Sprite rotation!

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -9,6 +10,8 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     clock = pygame.time.Clock()
     dt = 0
@@ -21,9 +24,13 @@ def main():
                 return
 
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
+
+    
 
 
 if __name__ == "__main__":
